3ebc7f5d14ea0e36c6191f9fff959b443414edb2
Maitrey1302 <64709380+Maitrey1302@users.noreply.github.com>
2020-06-02 13:19:46 +0530

    token and operators

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,product;
+	cout<<"enter a no a:";
+	cin>>a;
+	cout<<"enter a on b:";
+	cin>>b;
+	product=a*b;
+	cout<<"ans is:"<<product;
+	
+ } 
